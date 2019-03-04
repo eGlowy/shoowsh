03429369daf17f2c066719777cdcae1d225ab91b
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-04 17:06:31 +0200

    Lesson 6 in lesson

--- app/src/main/java/com/example/edgarsc/WelcomeActivity.kt
+++ app/src/main/java/com/example/edgarsc/WelcomeActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +13,7 @@ class WelcomeActivity : AppCompatActivity() {
 
         getStartedBtn.setOnClickListener {
             //explicit intent lai pateiktu kur konkrēti doties.
-            //implicit intent pasaka jebkuru aplikāciju ar kaut kādu kritēriju vērt vaļā
+            //implicit intent pasaka jebkuru aplikāciju ar kaut kādu kritēriju vērt vaļā (piemēram epasta apps, browseri uz linkiem u.t.t)
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
