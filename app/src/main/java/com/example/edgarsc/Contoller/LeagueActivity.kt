b8ff500717a372a1bae09fc067a7c80c5a90447a
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-06 13:00:54 +0200

    Lesson 10 added xlarge layour for tablets, added parcelable class for data transfer trough activitys

--- app/src/main/java/com/example/edgarsc/Contoller/LeagueActivity.kt
+++ app/src/main/java/com/example/edgarsc/Contoller/LeagueActivity.kt
@@ -13,9 +13,24 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("","")
 
+    //Saglabā player state , lai apgriežot ierīci nepazūd current state aktivitātei
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    //kad pagriež device notiek resotre aktivitātei un saglabāto player state padod jaunajai aktivitātes instancei, lai netiktu pazaudēti dati
+    //If statement priekš safe call , lai pārliecinātos ka dati tiešām ir, ja nav tad nedara neko un turpina no nulles.
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onMensClicked(view: View){
