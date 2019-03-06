d720046184e3c1f232c1d0df8b9f5d2c73d70c09
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-06 11:14:56 +0200

    Lesson 9 created player class for parcelable reason to pass data between activitys.

--- app/src/main/java/com/example/edgarsc/Contoller/LeagueActivity.kt
+++ app/src/main/java/com/example/edgarsc/Contoller/LeagueActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.edgarsc.Utilities.EXTRA_LEAGUE
+import com.example.edgarsc.Model.Player
 import com.example.edgarsc.R
+import com.example.edgarsc.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("","")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -19,27 +21,27 @@ class LeagueActivity : BaseActivity() {
     fun onMensClicked(view: View){
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked= false
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View){
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked= false
-        selectedLeague = "womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View){
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View)
     {
-        if (selectedLeague != "")
+        if (player.league != "")
         {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         }
         else
--- app/src/main/java/com/example/edgarsc/Contoller/SkillActivity.kt
+++ app/src/main/java/com/example/edgarsc/Contoller/SkillActivity.kt
@@ -4,37 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.edgarsc.Utilities.EXTRA_LEAGUE
+import com.example.edgarsc.Model.Player
 import com.example.edgarsc.R
-import com.example.edgarsc.Utilities.EXTRA_SKILL
+import com.example.edgarsc.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
     var selectedSkill = ""
     var league = ""
+    lateinit var player : Player
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
-        league = intent.getStringExtra(EXTRA_LEAGUE)
+        player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
 
     fun onBallerClicked(view: View){
         begginerSkillBtn.isChecked = false
-        selectedSkill = "baller"
+        player.skill = "baller"
     }
 
     fun onBegginerClicked(view: View){
         ballerSkilllBtn.isChecked = false
-        selectedSkill = "begginer"
+        player.skill = "begginer"
     }
 
     fun onSkillFinishClicked(view: View){
-        if (selectedSkill != "")
+        if (player.skill != "")
         {
             var finishActivity = Intent(this, FinishActivity::class.java)
-            finishActivity.putExtra(EXTRA_LEAGUE,league)
-            finishActivity.putExtra(EXTRA_SKILL, selectedSkill)
+            finishActivity.putExtra(EXTRA_PLAYER, player)
+
             startActivity(finishActivity)
         } else
         {
